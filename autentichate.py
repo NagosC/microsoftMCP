d37bf2c77639591ed56bf60611f0cef41d606118
NagosC <nagochiplay@hotmail.com>
2025-10-16 15:14:41 -0300

    Atulizando o property

--- autentichate.py
+++ autentichate.py
@@ -5,10 +5,6 @@ Run this script to sign in to one or more Microsoft accounts.
 """
 import os
 import sys
-from pathlib import Path
-
-# Add src to path so we can import our modules
-sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from dotenv import load_dotenv
 from microsoft_mcp import auth
